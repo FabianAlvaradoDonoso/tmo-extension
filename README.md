6b4909da5288258564b1f38031aaf3d346bdf36a
FabianAlvaradoDonoso <fabian.alvarado1008@gmail.com>
2025-02-21 10:36:20 -0300

    First commit 🚀

---
+++ README.md
@@ -0,0 +1,41 @@
+# TMO Next Chapter Extension
+
+## 🇪🇸 Español
+
+### Descripción
+
+TMO Next Chapter es una extensión para Chrome que mejora tu experiencia de lectura en TuMangaOnline (zonatmo.com). Añade un botón que te permite saltar instantáneamente al siguiente capítulo no leído de un manga.
+
+### Características
+
+- ✨ Añade un botón "Leer siguiente capítulo" en la página del manga
+- 🎯 Detecta automáticamente el siguiente capítulo no leído
+- 🚀 Abre el capítulo en una nueva pestaña
+- 💡 Si todos los capítulos están marcados como leídos, te permite volver al último capítulo
+
+### Instalación
+
+1. Visita la [Chrome Web Store](https://chrome.google.com/webstore)
+2. Busca "TMO Next Chapter" o [click aquí](https://chromewebstore.google.com/detail/tmo-next-chapter/ghmphghhlnpepmliaibljoiojopgnkhf?hl=es)
+3. Haz clic en "Añadir a Chrome"
+4. Confirma la instalación
+
+## 🇺🇸 English
+
+### Description
+
+TMO Next Chapter is a Chrome extension that enhances your reading experience on TuMangaOnline (zonatmo.com). It adds a button that lets you instantly jump to the next unread chapter of a manga.
+
+### Features
+
+- ✨ Adds a "Read next chapter" button on the manga page
+- 🎯 Automatically detects the next unread chapter
+- 🚀 Opens the chapter in a new tab
+- 💡 If all chapters are marked as read, allows you to return to the last chapter
+
+### Installation
+
+1. Visit the [Chrome Web Store](https://chrome.google.com/webstore)
+2. Search for "TMO Next Chapter" or [click here](https://chromewebstore.google.com/detail/tmo-next-chapter/ghmphghhlnpepmliaibljoiojopgnkhf?hl=es)
+3. Click "Add to Chrome"
+4. Confirm the installation
---
+++ content.js
@@ -0,0 +1,100 @@
+;(function removeSocialSideBar() {
+  // Buscamos el contenedor con id "chapters"
+  const title = 'TMO Extension:'
+
+  const chapters = document.querySelector('#chapters')
+  if (!chapters) {
+    console.log(title, 'No se encontró el div con id "chapters".')
+  } else {
+    // Dentro de chapters, buscamos el primer <ul>
+    const uls = chapters.querySelectorAll('.upload-link')
+
+    if (uls.length === 0) {
+      console.log(title, 'No se encontró capítulos.')
+    } else {
+      // Obtenemos todos los li
+      const liElements = [...uls]
+
+      // Creamos un array de booleans, inicializado en false
+      let viewedArray = new Array(liElements.length).fill(false)
+
+      // Recorremos cada <li> para ver si está "viewed"
+      liElements.forEach((li, index) => {
+        const span = li.querySelector('span.chapter-viewed-icon')
+
+        if (span && span.classList.contains('viewed')) {
+          viewedArray[index] = true
+        }
+      })
+
+      // Obtenemos el primer índice que tenga valor 'true'
+      const firstTrueIndex = viewedArray.indexOf(true) - 1
+      let targetLi, capNumber, isLast
+
+      if (firstTrueIndex === -1) {
+        // estan todos marcados como 'viewed', por lo que seleccionamos el primero
+        targetLi = liElements[0]
+        isLast = true
+      } else if (firstTrueIndex === -2) {
+        // no hay ninguno marcado como 'viewed', por lo que seleccionamos el último
+        targetLi = liElements[liElements.length - 1]
+      } else {
+        targetLi = liElements[firstTrueIndex]
+      }
+
+      const nombre = targetLi.querySelector('h4').textContent.trim()
+      const match = nombre.match(/Capítulo\s+(\d+(?:\.\d+)?)/i)
+      if (match) {
+        capNumber = match[1]
+      } else {
+        console.log(title, 'No se encontró el número de capítulo.')
+      }
+
+      let finalA = targetLi.querySelectorAll('li.list-group-item')
+      if (finalA.length === 0) {
+        console.log(title, 'No se encontró el enlace.')
+      }
+
+      finalA = finalA[0]
+        .querySelector('div')
+        .querySelector(':scope > div:nth-child(6)')
+        .querySelector('a')
+
+      if (targetLi) {
+        // 1. Buscar el botón (o enlace) que dice "Subir capítulo"
+        const subirCapituloBtn = Array.from(
+          document.querySelectorAll('a')
+        ).find((link) => link.textContent.trim() === 'Subir capítulo')
+
+        const textContext = isLast
+          ? `Volver a leer ultimo capítulo: ${capNumber}`
+          : `Leer siguiente capítulo: ${capNumber}`
+
+        // 2. Si encontramos "Subir capítulo", creamos e insertamos el nuevo botón
+        if (subirCapituloBtn) {
+          const icon = document.createElement('i')
+          icon.classList.add('fa', 'fa-step-forward')
+
+          const nuevoBoton = document.createElement('button')
+          nuevoBoton.textContent = ` ${textContext}`
+          nuevoBoton.classList.add('btn', 'btn-primary', 'btn-lg', 'btn-block')
+          nuevoBoton.addEventListener('click', () => {
+            window.open(finalA.href, '_blank')
+          })
+
+          // agregar <i> al principio del boton y después el texto
+          nuevoBoton.insertAdjacentElement('afterbegin', icon)
+
+          subirCapituloBtn.insertAdjacentElement('afterend', nuevoBoton)
+        } else {
+          console.log(
+            title,
+            'No se encontró un enlace con texto "Subir capítulo".'
+          )
+        }
+      } else {
+        console.log(title, 'No se encontró ningún <li> en la lista.')
+      }
+    }
+  }
+})()
